912522f0ac9854db87e4e7d6a7e61b1cb60ada0a
David Parish <dparish@gmail.com>
2023-03-21 11:57:41 -0500

    more

--- apps/nest-js/src/app.module.ts
+++ apps/nest-js/src/app.module.ts
@@ -3,20 +3,23 @@ import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Author} from "entities";
+import {AuthorController} from "./controllers/author.controller";
+import {AuthorService} from "./services/author.service";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       database: 'books',
       entities: [Author],
-      synchronize: true,
-    })
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([Author])
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthorController],
+  providers: [AppService, AuthorService],
 })
 export class AppModule {}
